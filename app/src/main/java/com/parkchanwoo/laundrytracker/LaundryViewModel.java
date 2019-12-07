be264f7655c13853732ecdf8e62ed18ce6f592b4
Chan Woo <14cpark@gmail.com>
2019-12-06 19:39:22 -0800

    Add method addNewWardrobe() Change return type of getWardrobeLiveData() to LiveData

--- app/src/main/java/com/parkchanwoo/laundrytracker/LaundryViewModel.java
+++ app/src/main/java/com/parkchanwoo/laundrytracker/LaundryViewModel.java
@@ -2,6 +2,7 @@ package com.parkchanwoo.laundrytracker;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -11,7 +12,7 @@ public class LaundryViewModel extends ViewModel {
 	private String TAG = this.getClass().getSimpleName();
 	private MutableLiveData<ArrayList<Wardrobe>> wardrobesLiveData;
 
-	public MutableLiveData<ArrayList<Wardrobe>> getWardrobesLiveData() {
+	public LiveData<ArrayList<Wardrobe>> getWardrobesLiveData() {
 		Log.i(TAG, "getWardrobesLiveData()");
 		if (wardrobesLiveData == null) {
 			wardrobesLiveData = new MutableLiveData<>();
@@ -29,5 +30,11 @@ public class LaundryViewModel extends ViewModel {
 	protected void onCleared() {
 		super.onCleared();
 		Log.i(TAG, "LaundryViewModel destroyed");
+	}
+
+	public void addNewWardrobe() {
+		ArrayList<Wardrobe> temp = wardrobesLiveData.getValue();
+		temp.add(new Wardrobe());
+		wardrobesLiveData.setValue(temp);
 	}
 }
