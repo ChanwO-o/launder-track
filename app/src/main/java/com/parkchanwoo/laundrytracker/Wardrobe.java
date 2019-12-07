85ef3108e80bc5728725408cc8b84882ae3bf63c
Chan Woo <14cpark@gmail.com>
2019-12-06 18:32:04 -0800

    Define objects

---
+++ app/src/main/java/com/parkchanwoo/laundrytracker/Wardrobe.java
@@ -0,0 +1,62 @@
+package com.parkchanwoo.laundrytracker;
+
+import java.util.ArrayList;
+
+public class Wardrobe {
+	private String name;
+	private ArrayList<ClothItem> clothItems;
+
+	public Wardrobe() {
+		name = "Unnamed Wardrobe";
+		clothItems = new ArrayList<>();
+	}
+
+	public Wardrobe(String name) {
+		this.name = name;
+		clothItems = new ArrayList<>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<ClothItem> getClothItems() {
+		return clothItems;
+	}
+
+	public void setClothItems(ArrayList<ClothItem> clothItems) {
+		this.clothItems = clothItems;
+	}
+
+	/**
+	 * Add new cloth item to wardrobe
+	 */
+	public void addClothItem(ClothItem clothItem) {
+		clothItems.add(clothItem);
+	}
+
+	/**
+	 * Remove cloth item from wardrobe
+	 */
+	public boolean removeClothItem(ClothItem clothItem) {
+		return clothItems.remove(clothItem);
+	}
+
+	/**
+	 * Clean out wardrobe
+	 */
+	public void clearWardrobe() {
+		clothItems.clear();
+	}
+
+	/**
+	 * Count number of cloth items in wardrobe
+	 */
+	public int getCount() {
+		return clothItems.size();
+	}
+}
