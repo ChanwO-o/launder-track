09671d0d897d8e2f85296d0570ed4b797772dc33
chanwoo <14cpark@gmail.com>
2020-02-17 13:16:07 -0800

    Hide splash screen showing in background of other Activities

--- app/src/main/java/com/parkchanwoo/laundrytracker/activities/EditClothItemActivity.java
+++ app/src/main/java/com/parkchanwoo/laundrytracker/activities/EditClothItemActivity.java
@@ -15,6 +15,7 @@ public class EditClothItemActivity extends AppCompatActivity implements ColorPic
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		setTheme(R.style.AppTheme);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_cloth_item);
 
